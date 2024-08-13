591b1a214d8fe6cbde4de8d5933ca2233fc6231d
Entruer <xinjia0104@gmail.com>
2024-08-13 19:06:45 +0900

    Add gyroscope connection.

--- main/main.c
+++ main/main.c
@@ -41,18 +41,19 @@ i2c_config_t i2c_config = {
 };
 
 uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .baud_rate  = 115200,
+        .data_bits  = UART_DATA_8_BITS,
+        .parity     = UART_PARITY_DISABLE,
+        .stop_bits  = UART_STOP_BITS_1,
+        .flow_ctrl  = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
 };
 
 /****************************************************************************************
  * Global Variables
  ****************************************************************************************/
 
-float Acceleration[3], Angle[3], Temperature;
+float Angle[3];
 
 /****************************************************************************************
  * Function Definitions
@@ -60,22 +61,23 @@ float Acceleration[3], Angle[3], Temperature;
 
 void DecodeIMUData(uint8_t chrTemp[])
 {
-  switch (chrTemp[1])
+  // Sum Check
+  uint8_t sum = 0;
+  for (int i = 0; i < 10; i++)
   {
-    case 0x51:
-      Acceleration[0] = ((uint16_t) (chrTemp[3] / 8 | chrTemp[2])) / 32768.0 * 16;
-      Acceleration[1] = ((uint16_t) (chrTemp[5] / 8 | chrTemp[4])) / 32768.0 * 16;
-      Acceleration[2] = ((uint16_t) (chrTemp[7] / 8 | chrTemp[6])) / 32768.0 * 16;
-      Temperature     = ((uint16_t) (chrTemp[9] / 8 | chrTemp[8])) / 340.0 + 36.53;
-      break;
-    case 0x53:
-      Angle[0]    = ((uint16_t) (chrTemp[3] / 8 | chrTemp[2])) / 32768.0 * 180;
-      Angle[1]    = ((uint16_t) (chrTemp[5] / 8 | chrTemp[4])) / 32768.0 * 180;
-      Angle[2]    = ((uint16_t) (chrTemp[7] / 8 | chrTemp[6])) / 32768.0 * 180;
-      Temperature = ((uint16_t) (chrTemp[9] / 8 | chrTemp[8])) / 340.0 + 36.53;
-      break;
-    default:
-      break;
+    sum += chrTemp[i];
+  }
+  if (sum != chrTemp[10])
+  {
+    return;
+  }
+
+  // Decode
+  if (chrTemp[0] == 0x55 && chrTemp[1] == 0x53)
+  {
+    Angle[0]    = ((uint16_t) (chrTemp[3] << 8 | chrTemp[2])) / 32768.0 * 180;
+    Angle[1]    = ((uint16_t) (chrTemp[5] << 8 | chrTemp[4])) / 32768.0 * 180;
+    Angle[2]    = ((uint16_t) (chrTemp[7] << 8 | chrTemp[6])) / 32768.0 * 180;
   }
 }
 
@@ -85,19 +87,16 @@ void DecodeIMUData(uint8_t chrTemp[])
 
 static void uart_task(void *args)
 {
-  uart_port_t uart_num = UART_PORT;
-  uint8_t    *data     = (uint8_t *) malloc(UART_BUFFER_SIZE);
+  uint8_t *data = (uint8_t *) malloc(UART_BUFFER_SIZE);
   while (1)
   {
-    int len = uart_read_bytes(uart_num, data, 30, 100);
+    int len = uart_read_bytes(UART_PORT, data, 11, 10);
     if (len > 0)
     {
-      DecodeIMUData(&data[0]);
-      DecodeIMUData(&data[20]);
-      ESP_LOGI("UART", "Gyroscope: X: %f, Y: %f, Z: %f", Angle[0], Angle[1], Angle[2]);
+      DecodeIMUData(data);
+      ESP_LOGI("IMU", "Angle: %.2f, %.2f, %.2f", Angle[0], Angle[1], Angle[2]);
     }
-
-    vTaskDelay(pdMS_TO_TICKS(20));
+    vTaskDelay(pdMS_TO_TICKS(5));
   }
 }
 
@@ -111,12 +110,12 @@ void app_main(void)
   ESP_ERROR_CHECK(gpio_reset_pin(LED_PIN));
   ESP_ERROR_CHECK(gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT));
 
-  ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_PORT, &i2c_config));
   ESP_ERROR_CHECK(i2c_driver_install(I2C_MASTER_PORT, I2C_MODE_MASTER, 0, 0, 0));
+  ESP_ERROR_CHECK(i2c_param_config(I2C_MASTER_PORT, &i2c_config));
 
+  ESP_ERROR_CHECK(uart_driver_install(UART_PORT, UART_BUFFER_SIZE, 0, 0, NULL, 0));
   ESP_ERROR_CHECK(uart_param_config(UART_PORT, &uart_config));
   ESP_ERROR_CHECK(uart_set_pin(UART_PORT, UART_TX_PIN, UART_RX_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-  ESP_ERROR_CHECK(uart_driver_install(UART_PORT, UART_BUFFER_SIZE, 0, 0, NULL, 0));
 
   xTaskCreate(uart_task, "uart_task", 2048, NULL, 10, NULL);
 
